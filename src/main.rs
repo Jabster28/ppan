eb735eaa85e26150abd2a04809a836c5a9da3af2
Jabster28 <justynboyer@gmail.com>
2022-09-29 22:37:31 +0100

    feat: changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::Inspectable;
+use bevy_rapier2d::prelude::{Velocity, *};
 #[cfg(feature = "discord")]
 use discord_game_sdk::Discord;
 // mod input_handlers;
@@ -43,9 +44,6 @@ struct Paddle;
 struct RotationVelocity(f32);
 
 #[derive(Inspectable, Component)]
-struct Velocity(f32, f32);
-
-#[derive(Inspectable, Component)]
 struct Acceleration(f32);
 
 #[derive(Inspectable, Component)]
@@ -57,7 +55,6 @@ struct Rotating(RotatingM);
 #[derive(Bundle)]
 struct PaddleBundle {
     rotation_velocity: RotationVelocity,
-    velocity: Velocity,
     acceleration: Acceleration,
     next_stop: NextStop,
     rotating: Rotating,
@@ -69,8 +66,12 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins)
         .add_plugin(InputManagerPlugin::<Action>::default())
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        // .add_plugin(RapierDebugRenderPlugin::default())
         .add_state(AppState::InGame)
         .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(setup_game))
+        .add_startup_system(setup)
+        // .add_system_set(SystemSet::on_enter(AppState::MainMenu).with_system(setup))
         .add_system_set(SystemSet::on_update(AppState::InGame).with_system(movement));
     // .add_plugin(WorldInspectorPlugin::new())
     #[cfg(feature = "discord")]
@@ -81,21 +82,18 @@ fn main() {
 
     app.run();
 }
-fn setup_game(
+
+fn setup(
     mut commands: Commands,
-    _meshes: ResMut<Assets<Mesh>>,
-    _materials: ResMut<Assets<ColorMaterial>>,
-    server: Res<AssetServer>,
     mut server_settings: ResMut<AssetServerSettings>,
+    mut rapier_config: ResMut<RapierConfiguration>,
+    server: Res<AssetServer>,
 ) {
-    let default_map = InputMap::new([
-        (KeyCode::A, Action::Left),
-        (KeyCode::D, Action::Right),
-        (KeyCode::W, Action::Up),
-        (KeyCode::S, Action::Down),
-        (KeyCode::C, Action::RotateAntiClockwise),
-        (KeyCode::V, Action::RotateClockwise),
-    ]);
+    rapier_config.gravity = Vec2::new(0.0, 0.0);
+    commands.spawn_bundle(Camera2dBundle::default());
+    #[cfg(debug_assertions)]
+    server.watch_for_changes().unwrap();
+
     server_settings.asset_folder =
     // chanegs for each os
     if cfg!(target_os = "windows") || cfg!(target_os = "linux") || cfg!(debug_asdsertions){
@@ -105,15 +103,16 @@ fn setup_game(
     } else {
         panic!("unsupported os")
     }.to_string();
-    let font: Handle<Font> = server.load("Blazma/Blazma-Regular.ttf");
+    let blazma: Handle<Font> = server.load("Blazma/Blazma-Regular.ttf");
+    let noto_sans: Handle<Font> =
+        server.load("Noto_Sans_Mono/NotoSansMono-VariableFont_wdth,wght.ttf");
 
-    commands.spawn_bundle(Camera2dBundle::default());
     commands.spawn_bundle(Text2dBundle {
         // set font
         text: Text::from_section(
-            "Hello World",
+            "/ppɒŋ/",
             TextStyle {
-                font,
+                font: noto_sans,
                 font_size: 40.0,
                 color: Color::WHITE,
             },
@@ -125,12 +124,22 @@ fn setup_game(
 
         ..Default::default()
     });
+}
+fn setup_game(mut commands: Commands) {
+    let default_map = InputMap::new([
+        (KeyCode::A, Action::Left),
+        (KeyCode::D, Action::Right),
+        (KeyCode::W, Action::Up),
+        (KeyCode::S, Action::Down),
+        (KeyCode::C, Action::RotateAntiClockwise),
+        (KeyCode::V, Action::RotateClockwise),
+    ]);
+
     for _ in 0..1 {
         // let mut commands = world.get_resource_mut::<Commands>().unwrap();
         commands
             .spawn_bundle(PaddleBundle {
                 rotation_velocity: RotationVelocity(0.0),
-                velocity: Velocity(0.0, 0.0),
                 acceleration: Acceleration(10.0),
                 next_stop: NextStop(0.0),
                 rotating: Rotating(RotatingM::Neither),
@@ -149,7 +158,16 @@ fn setup_game(
                 action_state: ActionState::default(),
                 // Describes how to convert from player inputs into those actions
                 input_map: default_map.clone(),
-            });
+            })
+            .insert(RigidBody::Dynamic)
+            .insert(Collider::cuboid(5.0, 25.0))
+            .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)));
+        commands
+            .spawn()
+            .insert(RigidBody::Dynamic)
+            .insert(Collider::ball(50.0))
+            .insert(Restitution::coefficient(1.0))
+            .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
         // world.resource_scope(|_, mut table: Mut<Table>| {
         // table.paddles[i].push(paddle);
         // });
@@ -160,156 +178,150 @@ fn movement(
     mut query: Query<
         (
             &ActionState<Action>,
-            &mut Transform,
-            &mut Velocity,
             &mut Acceleration,
             &mut NextStop,
             &mut Rotating,
-            &mut RotationVelocity,
+            &mut ExternalImpulse,
         ),
         With<Paddle>,
     >,
     _time: Res<Time>,
 ) {
-    for (
-        action_state,
-        mut transform,
-        mut velocity,
-        acceleration,
-        mut next_stop,
-        mut rotating,
-        rotation_velocity,
-    ) in query.iter_mut()
-    {
-        let delta_time = 16.0 / 1000.0;
-        let friction = 0.1;
-        // cpnvert to degrees
-        let rotation = transform.rotation.xyz().z;
-        let rotation = {
-            let mut newrot = rotation;
-            while newrot < 0.0 {
-                newrot += 360.0;
-            }
-            newrot % 360.0
-        };
+    for (action_state, acceleration, mut next_stop, mut rotating, mut impulse) in query.iter_mut() {
+        println!("aaa");
+        // let delta_time = 16.0 / 1000.0;
+        // let friction = 0.1;
+        // // cpnvert to degrees
+        // let rotation = transform.rotation.xyz().z;
+        // let rotation = {
+        //     let mut newrot = rotation;
+        //     while newrot < 0.0 {
+        //         newrot += 360.0;
+        //     }
+        //     newrot % 360.0
+        // };
 
-        println!("rotation: {}", rotation);
+        // println!("rotation: {}", rotation);
 
-        let _rot_accel = 0.8;
-        let (width, _height) = (10.0, 10.0);
+        // let _rot_accel = 0.8;
+        // let (width, _height) = (10.0, 10.0);
+        let mut vel = (0.0, 0.0);
 
         if action_state.pressed(Action::Right) {
-            velocity.0 += acceleration.0;
+            vel.0 += acceleration.0;
             // cap velocity to 1500
-            if velocity.0 > 1500.0 {
-                velocity.0 = 1500.0;
+            if vel.0 > 1500.0 {
+                vel.0 = 1500.0;
             }
+            println!("right {}", vel.0);
         }
         if action_state.pressed(Action::Left) {
-            velocity.0 -= acceleration.0;
+            vel.0 -= acceleration.0;
             // cap velocity to -1500
-            if velocity.0 < -1500.0 {
-                velocity.0 = -1500.0;
+            if vel.0 < -1500.0 {
+                vel.0 = -1500.0;
             }
         }
         if action_state.pressed(Action::Down) {
-            velocity.1 -= acceleration.0;
+            vel.1 -= acceleration.0;
             // cap velocity to -1500
-            if velocity.1 < -1500.0 {
-                velocity.1 = -1500.0;
+            if vel.1 < -1500.0 {
+                vel.1 = -1500.0;
             }
         }
         if action_state.pressed(Action::Up) {
-            velocity.1 += acceleration.0;
+            vel.1 += acceleration.0;
             // cap velocity to 1500
-            if velocity.1 > 1500.0 {
-                velocity.1 = 1500.0;
+            if vel.1 > 1500.0 {
+                vel.1 = 1500.0;
             }
         }
 
-        if (rotating.0 == RotatingM::AntiClockwise
-            && (rotation * 180.0 / std::f32::consts::PI - next_stop.0).abs() < 30.0)
-            || (rotating.0 == RotatingM::Clockwise
-                && (rotation * 180.0 / std::f32::consts::PI - next_stop.0).abs() < 30.0)
-        {
-            rotating.0 = RotatingM::Neither;
-        }
+        // if (rotating.0 == RotatingM::AntiClockwise
+        //     && (rotation * 180.0 / std::f32::consts::PI - next_stop.0).abs() < 30.0)
+        //     || (rotating.0 == RotatingM::Clockwise
+        //         && (rotation * 180.0 / std::f32::consts::PI - next_stop.0).abs() < 30.0)
+        // {
+        //     rotating.0 = RotatingM::Neither;
+        // }
 
-        if rotating.0 == RotatingM::Clockwise && rotating.0 == RotatingM::AntiClockwise {
-            // only keep cw
-            rotating.0 = RotatingM::Clockwise;
-        }
+        // if rotating.0 == RotatingM::Clockwise && rotating.0 == RotatingM::AntiClockwise {
+        //     // only keep cw
+        //     rotating.0 = RotatingM::Clockwise;
+        // }
 
-        if action_state.pressed(Action::RotateAntiClockwise) {
-            rotating.0 = RotatingM::AntiClockwise;
-            // get next 90 degree rotation to the left
-            next_stop.0 =
-                (90.0 * ((rotation * 180.0 / std::f32::consts::PI) / 90.0).floor()) as f32;
-            if (next_stop.0 - (rotation * 180.0 / std::f32::consts::PI)).abs() < f32::EPSILON {
-                next_stop.0 -= 90.0;
-            }
-            while next_stop.0 < 0.0 {
-                next_stop.0 += 360.0;
-            }
-            next_stop.0 %= 360.0;
-        }
+        // if action_state.pressed(Action::RotateAntiClockwise) {
+        //     rotating.0 = RotatingM::AntiClockwise;
+        //     // get next 90 degree rotation to the left
+        //     next_stop.0 =
+        //         (90.0 * ((rotation * 180.0 / std::f32::consts::PI) / 90.0).floor()) as f32;
+        //     if (next_stop.0 - (rotation * 180.0 / std::f32::consts::PI)).abs() < f32::EPSILON {
+        //         next_stop.0 -= 90.0;
+        //     }
+        //     while next_stop.0 < 0.0 {
+        //         next_stop.0 += 360.0;
+        //     }
+        //     next_stop.0 %= 360.0;
+        // }
 
-        if action_state.pressed(Action::RotateClockwise) {
-            rotating.0 = RotatingM::Clockwise;
+        // if action_state.pressed(Action::RotateClockwise) {
+        //     rotating.0 = RotatingM::Clockwise;
 
-            // get next 90 degree rotation to the right
-            next_stop.0 = (90.0 * (rotation * 180.0 / std::f32::consts::PI / 90.0).ceil()) as f32;
-            // check if same
-            if (next_stop.0 - rotation * 180.0 / std::f32::consts::PI).abs() < f32::EPSILON {
-                next_stop.0 += 90.0;
-            }
-            next_stop.0 %= 360.0;
-        }
+        //     // get next 90 degree rotation to the right
+        //     next_stop.0 = (90.0 * (rotation * 180.0 / std::f32::consts::PI / 90.0).ceil()) as f32;
+        //     // check if same
+        //     if (next_stop.0 - rotation * 180.0 / std::f32::consts::PI).abs() < f32::EPSILON {
+        //         next_stop.0 += 90.0;
+        //     }
+        //     next_stop.0 %= 360.0;
+        // }
 
-        // #[cfg(debug_assertions)]
-        println!(
-            "next stop is {}, i'm at {} so i wanna set velocity to {}",
-            next_stop.0, rotation, rotation_velocity.0
-        );
-        transform.rotate_z(rotation_velocity.0 * std::f32::consts::PI / 180.0);
-
-        // if the paddle's rotating right, its rotational velocity should also decrease as it reaches the next 90 degree mark
+        // // #[cfg(debug_assertions)]
         // println!(
-        //     "x: {: >4} y: {: >4} gl: {: >5} gr: {: >5} rot: {: >4} rotvel: {: >4} nxtstop: {: >4} fps: {: >4}",
-        //     // pad start to 3 chars
-        //     x.round(),
-        //     y.round(),
-        //     rotating.0 == RotatingM::AntiClockwise,
-        //     rotating.0 == RotatingM::Clockwise,
-        //     rotation.round(),
-        //     rotation_velocity.round(),
-        //     next_stop.round(),
-        //     ggez::timer::fps(_ctx).round()
-        // );!!
+        //     "next stop is {}, i'm at {} so i wanna set velocity to {}",
+        //     next_stop.0, rotation, rotation_velocity.0
+        // );
+        // // TODO: fix this (lol have fun)
+        // transform.rotate_z(rotation_velocity.0 * std::f32::consts::PI / 180.0);
 
-        // speed calculations
-        transform.translation.x += velocity.0 * delta_time;
-        velocity.0 *= 1.0 - friction;
+        // // if the paddle's rotating right, its rotational velocity should also decrease as it reaches the next 90 degree mark
+        // // println!(
+        // //     "x: {: >4} y: {: >4} gl: {: >5} gr: {: >5} rot: {: >4} rotvel: {: >4} nxtstop: {: >4} fps: {: >4}",
+        // //     // pad start to 3 chars
+        // //     x.round(),
+        // //     y.round(),
+        // //     rotating.0 == RotatingM::AntiClockwise,
+        // //     rotating.0 == RotatingM::Clockwise,
+        // //     rotation.round(),
+        // //     rotation_velocity.round(),
+        // //     next_stop.round(),
+        // //     ggez::timer::fps(_ctx).round()
+        // // );!!
 
-        transform.translation.y += velocity.1 * delta_time;
-        velocity.1 *= 1.0 - friction;
+        // // speed calculations
+        // transform.translation.x += velocity.0 * delta_time;
+        // velocity.0 *= 1.0 - friction;
 
-        // ensure transform.translation.0 is in bounds, and reset velocittransform.translation.1if it is
-        if transform.translation.x < width / 2.0 {
-            transform.translation.x = width / 2.0;
-            velocity.0 = 0.0;
-        } else if transform.translation.x > 800.0 - width / 2.0 {
-            transform.translation.x = 800.0 - width / 2.0;
-            velocity.0 = 0.0;
-        }
-        // 0 >transform.translation.y> 600
-        if transform.translation.y < 0.0 {
-            transform.translation.y = 0.0;
-            velocity.1 = 0.0;
-        } else if transform.translation.y > 600.0 {
-            transform.translation.y = 600.0;
-            velocity.1 = 0.0;
-        }
+        // transform.translation.y += velocity.1 * delta_time;
+        // velocity.1 *= 1.0 - friction;
+        impulse.impulse = Vec2::new(vel.0, vel.1);
+
+        // // ensure transform.translation.0 is in bounds, and reset velocittransform.translation.1if it is
+        // if transform.translation.x < width / 2.0 {
+        //     transform.translation.x = width / 2.0;
+        //     velocity.0 = 0.0;
+        // } else if transform.translation.x > 800.0 - width / 2.0 {
+        //     transform.translation.x = 800.0 - width / 2.0;
+        //     velocity.0 = 0.0;
+        // }
+        // // 0 >transform.translation.y> 600
+        // if transform.translation.y < 0.0 {
+        //     transform.translation.y = 0.0;
+        //     velocity.1 = 0.0;
+        // } else if transform.translation.y > 600.0 {
+        //     transform.translation.y = 600.0;
+        //     velocity.1 = 0.0;
+        // }
     }
 }
 #[cfg(feature = "discord")]
@@ -332,6 +344,7 @@ fn setup_discord(world: &mut World) {
 #[cfg(feature = "discord")]
 
 fn discord_update(discord: Option<NonSendMut<DiscordState>>) {
+    // TODO: add more states
     if discord.is_none() {
         return;
     }
@@ -341,6 +354,7 @@ fn discord_update(discord: Option<NonSendMut<DiscordState>>) {
     let activity = activity
         // party status
         .with_state("idle")
+        .with_large_image_key("logo")
         // player status
         .with_details("in the menus");
     discord.0.update_activity(activity, |_, result| {
